ef1b245ce989f87b1d2b05fb713cc74a2807e9b9
Jijidici <jdefaye.jijidici@gmail.com>
2013-03-24 15:34:21 +0100

    Add void paintTheSky() test function

--- include/display/procedural_sky.hpp
+++ include/display/procedural_sky.hpp
@@ -1,6 +1,9 @@
 #ifndef __PROCEDURAL_SKY_HPP__
 #define __PROCEDURAL_SKY_HPP__
 
+#include <GL/glew.h>
 
+/* Test for dynamique texturing the sky */
+void paintTheSky(GLuint texID);
 
 #endif
--- src_display/procedural_sky.cpp
+++ src_display/procedural_sky.cpp
@@ -1,1 +1,8 @@
 #include "display/procedural_sky.hpp"
+
+#include <GL/glew.h>
+
+/* Test for dynamique texturing the sky */
+void paintTheSky(GLuint texID){
+	
+}
