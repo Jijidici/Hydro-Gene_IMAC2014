7d6230c25645f9efd18302ee17226ea3a302c73e
Jijidici <jdefaye.jijidici@gmail.com>
2013-03-24 15:41:31 +0100

    Add a loadProgram() function with just 2 shaders

--- include/tools/shader_tools.hpp
+++ include/tools/shader_tools.hpp
@@ -8,6 +8,10 @@ namespace hydrogene {
 // Charge, compile et link les fichier sources GLSL passés en paramètre. Renvoit l'identifiant du programme compilé.
 GLuint loadProgram(const char* vertexShaderFile, const char* fragmentShaderFile, const char* geometryShaderFile);
 
+// Charge, compile et link les fichier sources GLSL passés en paramètre. Renvoit l'identifiant du programme compilé.
+GLuint loadProgram(const char* vertexShaderFile, const char* fragmentShaderFile);
+
+
 }
 
 #endif
